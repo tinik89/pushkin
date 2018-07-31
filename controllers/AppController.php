0fa39714b25a35a85fff12be6848071c16307780
tinik89 <6789.tinik@gmail.com>
2018-07-31 23:07:28 +0300

    empty controller AppController

---
+++ controllers/AppController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TIN
+ * Date: 31.07.2018
+ * Time: 22:59
+ */
+
+namespace app\controllers;
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+
+}
